cefc5dddec00fa99f1eed571e96bdf402ac6d474
Anishaa De Silva <AnishaaDeSilva@Anishaa-3.fios-router.home>
2020-03-15 19:04:31 -0400

    adding airline ETL project files

---
+++ Resources/airlines.sql
@@ -0,0 +1,12 @@
+-- Create table for to load the data into the db
+CREATE TABLE "airline_route_data" (
+id INT PRIMARY KEY,
+"Airline_Name" TEXT,
+"Airline_Code" TEXT,
+"Departure_Airport" TEXT,
+"Departure_Airport_Code" TEXT,
+"Departure_Country" TEXT,
+"Destination_Airport" TEXT,
+"Destination_Airport_Code" TEXT,
+"Destination_Country" TEXT
+);
